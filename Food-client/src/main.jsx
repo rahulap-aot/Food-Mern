cd7be5a82fc81dfe798f0139df9008bc1151f241
ArronRahul <rahul.ap@aot-technologies.com>
2024-09-10 05:35:06 -0700

    today

--- Food-client/src/main.jsx
+++ Food-client/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from'react-router-dom'
+import router from './router/Router'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />,
 )
