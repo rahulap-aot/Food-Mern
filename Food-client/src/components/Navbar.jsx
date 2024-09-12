68b8d9af468ca65df8ea00905e79a900c5b684fc
ArronRahul <rahul.ap@aot-technologies.com>
2024-09-12 03:57:50 -0700

    images

--- Food-client/src/components/Navbar.jsx
+++ Food-client/src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">{logo}</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
